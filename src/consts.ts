18af3077bbd7208f76554a32f1112d1ee744cb04
SIG <gasper.sircelj@metronik.si>
2026-08-18 08:45:02 +0200

    icon and some other things

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gašper Šircelj';
-export const SITE_DESCRIPTION = 'Personal website of Gašper Šircelj.';
+export const SITE_TITLE = 'Gašper Šircelj — Pharma consultant & writer';
+export const SITE_DESCRIPTION = 'CS engineer turned pharma consultant. Cycled Slovenia to Singapore. Writing about what I\'m learning along the way.';

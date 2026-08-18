e1ddc9fd09e01b55e72c9ccdd69fb15b3f556c05
SIG <gasper.sircelj@metronik.si>
2026-08-18 08:21:55 +0200

    Update name, gradients, animations, about text, recent posts section

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gašper Jamnik';
-export const SITE_DESCRIPTION = 'Personal website of Gašper Jamnik.';
+export const SITE_TITLE = 'Gašper Šircelj';
+export const SITE_DESCRIPTION = 'Personal website of Gašper Šircelj.';
